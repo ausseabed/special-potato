377d461127f8e48e583c81083a7bee88da94aea5
Josh Sixsmith <josh.sixsmith@gmail.com>
2021-09-20 14:22:58 +1000

    Added a stac metadata function

--- NIDEM-samples/nidem_convert.py
+++ NIDEM-samples/nidem_convert.py
@@ -25,6 +25,7 @@ from pystac.extensions.pointcloud import (
 )
 from shapely.geometry import box, mapping
 from shapely import wkt
+import uritools
 import structlog
 
 _LOG = structlog.get_logger()
@@ -167,3 +168,95 @@ def hexbin(data_uri: str, config_pathname: Path, out_pathname: Path) -> Dict[str
         json.dump(metadata, src, indent=4)
 
     return metadata
+
+
+def stac_metadata(
+    info_metadata: Dict[str, Any],
+    stats_metadata: Dict[str, Any],
+    hex_metadata: Dict[str, Any],
+    data_uri: uritools.split.SplitResultString,
+    out_pathname: Path,
+) -> None:
+    """Generate STAC metadata"""
+    # see https://cmi.ga.gov.au/data-products/dea/325/dea-intertidal-elevation-landsat
+    # for datetime information
+    end_datetime = parser.parse("2017-07-31T23:59:59.00Z")
+    start_datetime = parser.parse("1986-08-16T00:00:00.00Z")
+    created = datetime.now(timezone.utc)
+
+    bounding_box = [
+        info_metadata["metadata"]["filters.info"]["bbox"]["minx"],
+        info_metadata["metadata"]["filters.info"]["bbox"]["miny"],
+        info_metadata["metadata"]["filters.info"]["bbox"]["minz"],
+        info_metadata["metadata"]["filters.info"]["bbox"]["maxx"],
+        info_metadata["metadata"]["filters.info"]["bbox"]["maxy"],
+        info_metadata["metadata"]["filters.info"]["bbox"]["maxz"],
+    ]
+    # geometry = box(
+    #     minx=bounding_box[0],
+    #     miny=bounding_box[1],
+    #     maxx=bounding_box[3],
+    #     maxy=bounding_box[4],
+    # )  # incase a bounding box is preferred for spatially referencing datasets
+    geometry = wkt.loads(hex_metadata["metadata"]["filters.hexbin"]["boundary"])
+
+    item = pystac.Item(
+        id=str(uuid.uuid4()),
+        datetime=end_datetime,  # just using the end date as NIDEM is a time composition
+        geometry=mapping(geometry),
+        bbox=bounding_box,
+        properties={},
+    )
+
+    # common metadata
+    item.common_metadata.title = "NIDEM-25m"
+    item.common_metadata.description = "Sourced from NIDEM and converted to a point cloud"
+    item.common_metadata.start_datetime = start_datetime
+    item.common_metadata.end_datetime = end_datetime
+    item.common_metadata.created = created
+    item.common_metadata.instruments = [
+        "tm",
+        "etm+",
+        "oli",
+        "tirs",
+    ]
+    item.common_metadata.providers = [
+        pystac.Provider(
+            name="James Cook University", roles=[pystac.ProviderRole.PRODUCER]
+        ),
+        pystac.Provider(name="Geoscience Australia", roles=[pystac.ProviderRole.HOST]),
+    ]
+
+    proj = ProjectionExtension.ext(item, add_if_missing=True)
+    pc = PointcloudExtension.ext(item, add_if_missing=True)
+
+    proj.apply(epsg=4326)
+
+    # schema and stats metadata
+    pc_schema = info_metadata["metadata"]["filters.info"]["schema"]["dimensions"]
+    pc_stats = stats_metadata["metadata"]["filters.stats"]["statistic"]
+
+    # point cloud extension
+    pc.apply(
+        count=info_metadata["metadata"]["filters.info"]["num_points"],
+        type=PhenomenologyType.EOPC,
+        encoding="TileDB",
+        schemas=[Schema(properties=sch) for sch in pc_schema],
+        statistics=[Statistic(properties=stats) for stats in pc_stats],
+    )
+
+    # data and metadata uri population
+    metadata_pathname = Path(data_uri.path)
+    out_pathname = Path(metadata_pathname.parent, metadata_pathname.stem).with_suffix(".stac.json")
+    metadata_target = uritools.uricompose(scheme=data_uri.scheme, authorit=data_uri.authority, path=str(out_pathname))
+    item.add_asset("data", pystac.Asset(href=data_uri.geturi(), roles=["data"]))
+    item.add_link(
+        pystac.Link(rel="self", media_type=pystac.MediaType.JSON, target=metadata_target)
+    )
+
+    # ignore validation for this prototype
+    # item.validate()
+
+    _LOG.info("writing results from hexbin filter", pathname=out_pathname)
+    with open(out_pathname, "w") as src:
+        json.dump(item.to_dict(), src, indent=4)
